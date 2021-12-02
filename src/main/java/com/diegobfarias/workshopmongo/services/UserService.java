8c09724b39f50f281224c51370d76f8d7bf93d90
Diego Farias <fariasdb@outlook.com>
2021-12-02 14:27:17 -0300

    Obtendo um usuário por id

--- src/main/java/com/diegobfarias/workshopmongo/services/UserService.java
+++ src/main/java/com/diegobfarias/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.diegobfarias.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diegobfarias.workshopmongo.domain.User;
 import com.diegobfarias.workshopmongo.repositories.UserRepository;
+import com.diegobfarias.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
