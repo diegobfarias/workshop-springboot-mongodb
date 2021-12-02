0b369477a3f6036741adaf6a0c311fedf9e30171
Diego Farias <fariasdb@outlook.com>
2021-12-02 15:08:55 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/diegobfarias/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.diegobfarias.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.diegobfarias.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
