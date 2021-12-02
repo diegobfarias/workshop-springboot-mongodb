1b377c989b5d5d0389069ae75087d515148241e9
Diego Farias <fariasdb@outlook.com>
2021-12-02 15:31:48 -0300

    Obtendo um post por id

---
+++ src/main/java/com/diegobfarias/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.diegobfarias.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegobfarias.workshopmongo.domain.Post;
+import com.diegobfarias.workshopmongo.repositories.PostRepository;
+import com.diegobfarias.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
